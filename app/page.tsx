8faf43e7a8ae2f4d883b4fc484bfeb32bef116f9
Diego Elías <eliaslopezdiegojosue@gmail>
2026-09-22 23:14:33 -0600

    Información en page

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,24 @@ export default function Home(){
         </h1>
 
         <p className="mt-4 text-lg text-gray-600">
-          Mi primer proyecto demo con Nect.js
+          Odio hacer páginas web
         </p>
 
         <p className="mt-6 text-lg text-gray-500">
           Desplegando con Vercel.
+        </p>
+      </section>
+
+      <section className="text-left">
+        <p className="mt-4 text-lg text-black-600">
+          Estoy intentando...
+        </p>
+
+        <p className="mt-3 text-lg text-gray-300">
+          Es muy dificil de establecer una verdadera compatibilidad <br/>
+          con algo que has pasado intentando durante tanto tiempo <br/>
+          y ahora lo vez como una carga, pero es tu futuro, y no sabés <br/>
+          con eso...
         </p>
       </section>
     </main>
